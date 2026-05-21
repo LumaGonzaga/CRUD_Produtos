71d8a040230ecd3b3342d0f96ef6d85ae859d12f
Luma Quintanilha Gonzaga <luma2gonzaga@outlook.com.br>
2026-05-21 18:14:44 -0300

    Mapeamento da tabela e das colunas e implementação da Entidade Produto usando JPA Entity.

--- ProdutosAPI/ProdutosAPI/src/main/java/io/GitHub/LumaGonzaga/ProdutosAPI/model/Produto.java
+++ ProdutosAPI/ProdutosAPI/src/main/java/io/GitHub/LumaGonzaga/ProdutosAPI/model/Produto.java
@@ -1,11 +1,27 @@
 package io.GitHub.LumaGonzaga.ProdutosAPI.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity; // demonstrar que a classe produto é uma entidade JPA//  Antigo import javax.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 // POJO -> Plain Old Java Object
+@Entity//mapeia uma classe para uma tabela de banco de dados.
+@Table// Mesma coisa que para coluna, mas agora para o nome da tabela, nesse caso, o nome da tabela é o mesmo da classe, então não era necessário.
 public class Produto {
 
+    @Id// Usado para mostrar que o id é a chave primária da tabela. Que é uma coluna ou conjunto de colunas que identifica de forma única cada linha (ou registro) em uma tabela de banco de dados. Ela impede que duas linhas tenham os mesmos dados nesse campo e não permite valores em branco (nulos)
+    @Column// Serve para mapear as colunas no banco de dados. É usado passando o nome da variável que repesenta essa variável no banco de dados.
+    // EX: @Column(name = "código"), além disso podemos colocar "," depois de código e crtl + p, conseguimos ver outras propriedades que podemos aplicar ao valor dessa coluna . Não é obrigatório colocar o @Column para as variáveis que tem o mesmo nome no banco de dados, então nesse caso não precisaria
     private String id;
+
+    @Column
     private String nome;
+
+    @Column
     private String descricao;
+
+    @Column
     private Double preco;
 
     public String getId() {
